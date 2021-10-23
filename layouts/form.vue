8ca592035c11ddec6465140f1f3562f3c3e6e5bb
Freddy <freddyamarante14@gmail.com>
2021-10-23 14:37:34 -0500

    Initial commit

---
+++ layouts/form.vue
@@ -0,0 +1,8 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="12" :offset="6">
+        <h1>asdasdasdasds</h1>
+      <Nuxt />
+    </el-col>
+  </el-row>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-form ref="form" :model="form" label-width="120px">
+      <el-form-item label="Correo">
+        <el-input v-model="form.name" type="email"></el-input>
+      </el-form-item>
+      <el-form-item label="Contraseña">
+        <el-input v-model="form.password" type="password"></el-input>
+      </el-form-item>
+      <el-form-item label="Recordarme">
+        <el-checkbox v-model="rememberMe"></el-checkbox>
+      </el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button>Cancel</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  layout: 'form',
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+        rememberMe: false,
+      },
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!')
+    },
+  },
+}
+</script>
